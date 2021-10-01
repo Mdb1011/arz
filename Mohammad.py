344f2f297e6928798fb8d7450d510667ea49e043
Mdb1011 <89591649+Mdb1011@users.noreply.github.com>
2021-10-01 15:32:25 +0330

    Update Mohammad.py

--- Mohammad.py
+++ Mohammad.py
@@ -10,10 +10,10 @@ myjson = json.load(http)
 for data in myjson['data']
 
 
-    print(Fore.GREEN+"NAME : "+Fore.WHITE+ data[`name`])
-    print(Fore.GREEN+"Dollar price : "+Fore.WHITE+ data[`Dollar price`]
-    print(Fore.GREEN+"Toman price : "+Fore.WHITE+ data[`Toman price`]
-    print(Fore.GREEN+"Daily change : "+Fore.WHITE+ data[`Daily change`]
-    print(Fore.GREEN+"weekly change : "+Fore.WHITE+ data[`weekly change`]
+    print(Fore.GREEN+"NAME : "+Fore.WHITE+ data['name'])
+    print(Fore.GREEN+"Dollar price : "+Fore.WHITE+ data['Dollar price']
+    print(Fore.GREEN+"Toman price : "+Fore.WHITE+ data['Toman price']
+    print(Fore.GREEN+"Daily change : "+Fore.WHITE+ data['Daily change']
+    print(Fore.GREEN+"weekly change : "+Fore.WHITE+ data['weekly change']
 
     print(Fore.RED+"\n####################################################")
